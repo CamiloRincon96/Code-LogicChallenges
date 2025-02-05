4e2501aba95e364a6098e90f4facbce03f110c63
juan.rincon1 <Juan.Rincon2.-ND@disney.com>
2025-02-04 21:51:00 -0500

    added 1 more

--- src/ProjectEuler/MultiplesOf3Or5.java
+++ src/ProjectEuler/MultiplesOf3Or5.java
@@ -1,2 +1,23 @@
-package ProjectEuler;public class MultiplesOf3Or5 {
+package ProjectEuler;
+
+public class MultiplesOf3Or5 {
+
+    public static int getAllNumbersMultiplesOf3And5(int num) {
+
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (i%3 == 0 || i%5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(getAllNumbersMultiplesOf3And5(1000));
+
+    }
 }
