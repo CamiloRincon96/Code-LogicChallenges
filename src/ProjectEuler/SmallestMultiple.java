be5ce9cb21dda803b9c3d05ea8152ff9afe9866b
juan.rincon1 <Juan.Rincon2.-ND@disney.com>
2025-02-16 02:16:28 -0500

    This was a sad try with an Project Euler challenge

---
+++ src/ProjectEuler/SmallestMultiple.java
@@ -0,0 +1,21 @@
+package ProjectEuler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmallestMultiple {
+
+    public static void smallestMultiple() {
+
+        List<Integer> maxNumbers = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+
+
+        }
+    }
+    public static void main(String[] args) {
+
+
+        System.out.println();    }
+}
